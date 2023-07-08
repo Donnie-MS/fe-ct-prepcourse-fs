3a6efe50cb6b56315bdebfbf8b315a563c852180
Donnie-MS <joaomasgosandoval@gmail.com>
2023-07-08 01:02:11 -0300

    ejercicios

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -3,34 +3,53 @@
 function devolverPrimerElemento(array) {
    // Retornar el primer elemento del arreglo recibido por parámetro.
    // Tu código:
+   return array[0]
 }
 
 function devolverUltimoElemento(array) {
    // Retornar el último elemento del arreglo recibido por parámetro.
    // Tu código:
+   const lista = array
+   let length = lista.length
+   length = length - 1
+
+   return array[length]
 }
 
 function obtenerLargoDelArray(array) {
    // Retornar la longitud del arreglo recibido por parámetro.
    // Tu código:
+   let lista = array.length
+   return lista
 }
 
 function incrementarPorUno(array) {
    // El arreglo recibido por parámetro contiene números.
    // Retornar un arreglo con los elementos incrementados en +1.
    // Tu código:
+   const array2 = array
+   let sumar1
+   sumar1 = array2.map(function(elementos){
+      elementos = elementos + 1
+      return elementos
+   })
+   return sumar1
 }
 
 function agregarItemAlFinalDelArray(array, elemento) {
    // Agrega el "elemento" al final del arreglo recibido.
    // Retorna el arreglo.
    // Tu código:
+   array.push(elemento)
+   return array
 }
 
 function agregarItemAlComienzoDelArray(array, elemento) {
    // Agrega el "elemento" al comienzo del arreglo recibido.
    // Retorna el arreglo.
    // Tu código:
+   array.unshift(elemento)
+   return array
 }
 
 function dePalabrasAFrase(palabras) {
@@ -39,30 +58,45 @@ function dePalabrasAFrase(palabras) {
    // con un espacio entre cada palabra.
    // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'.
    // Tu código:
+   let juntar = palabras.join(` `)
+   
+   return juntar
 }
 
 function arrayContiene(array, elemento) {
    // Verifica si el elemento existe dentro del arreglo recibido.
    // Retornar true si está, o false si no está.
    // Tu código:
+   let buscar = array.includes(elemento)
+   return buscar
 }
 
 function agregarNumeros(arrayOfNums) {
    // El parámetro "arrayOfNums" debe ser un arreglo de números.
    // Suma todos los elementos y retorna el resultado.
    // Tu código:
+   let numeros = arrayOfNums
+   let total = numeros.reduce((a, b)=>a+b)
+   return total
 }
 
 function promedioResultadosTest(resultadosTest) {
    // El parámetro "resultadosTest" es un arreglo de números.
    // Itera (en un bucle) los elementos del arreglo y devuelve el promedio de las notas.
    // Tu código:
+   let total = 0
+   for (let index = 0; index < resultadosTest.length; index++) {
+      total +=resultadosTest[index]
+   }
+   return total / resultadosTest.length
 }
 
 function numeroMasGrande(arrayOfNums) {
    // El parámetro "arrayOfNums" es un arreglo de números.
    // Retornar el número más grande.
    // Tu código:
+   const max = arrayOfNums.reduce((a, b)=> Math.max(a, b))
+   return max
 }
 
 function multiplicarArgumentos() {
@@ -70,11 +104,27 @@ function multiplicarArgumentos() {
    // Si no se pasan argumentos retorna 0. Si se pasa un argumento, simplemente retórnalo.
    // [PISTA]: "arguments" es un arreglo.
    // Tu código:
+   if (arguments.length === 0) {
+      return 0;
+   }
+   var producto = 1;
+   for (var i = 0; i < arguments.length; i++) {
+      producto *= arguments[i];
+   }
+   return producto
 }
 
 function cuentoElementos(array) {
    // Desarrolla una función que retorne la cantidad de elementos del arreglo cuyo valor sea mayor que 18.
    // Tu código:
+   let respuesta = 0
+   for (let index = 0; index < array.length; index++) {
+      const element = array[index];
+      if (array[index] > 18) {
+         respuesta = respuesta + 1
+      }
+   }
+   return respuesta
 }
 
 function diaDeLaSemana(numeroDeDia) {
@@ -82,18 +132,36 @@ function diaDeLaSemana(numeroDeDia) {
    // Realiza una función que, dado el número del día de la semana, retorne: "Es fin de semana"
    // si el día corresponde a "Sábado" o "Domingo", y "Es dia laboral" en caso contrario.
    // Tu código:
+   if (numeroDeDia== 7 || numeroDeDia == 1) {
+      return "Es fin de semana"
+   }
+   else{
+      return "Es dia laboral"}
 }
 
 function empiezaConNueve(num) {
    // Esta función recibe por parámetro un número.
    // Debe retornar true si el entero inicia con 9 y false en otro caso.
    // Tu código:
+   let string = String(num)[0]
+   let numero = Number(string)
+   if (numero == 9) {
+      return true
+   } else {
+      return false
+   }
 }
 
 function todosIguales(array) {
    // Si todos los elementos del arreglo son iguales, retornar true.
    // Caso contrario retornar false.
    // Tu código:
+   if (array[0] == array[1] || array.length == 1) {
+      return true
+   }
+   else{
+      return false
+   }
 }
 
 function mesesDelAño(array) {
@@ -101,18 +169,41 @@ function mesesDelAño(array) {
    // "Marzo" y "Noviembre", guardarlos en un nuevo arreglo y retornarlo.
    // Si alguno de los meses no está, retornar el string: "No se encontraron los meses pedidos".
    // Tu código:
-}
+   let enero2 = array.includes("Enero")
+   let eneropos = array[0]
+   const meses = ["Marzo", "Noviembre", "Enero"]
+   if (enero2 == true && "Enero" == array[0]) {
+      return "Enero"
+   }
+   else if(enero2 == true){
+      return meses
+   }
+   else{
+      return "No se encontraron los meses pedidos"
+   }}
 
 function tablaDelSeis() {
    // Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
    // La función devuelve un arreglo con los resultados de la tabla de multiplicar del 6 en orden creciente.
    // Tu código:
+   let array = []
+   for (let index = 0; index <= 10; index++) {
+      array.push(6 * index)
+   }
+   return array
 }
 
 function mayorACien(array) {
    // La función recibe un arreglo con enteros entre 0 y 200.
    // Recorrerlo y retornar un arreglo con todos los valores mayores a 100 (no incluye el 100).
    // Tu código:
+   const total =[]
+   array.map(function(element){
+      if (element > 100) {
+         total.push(element)
+      }
+   })
+   return total
 }
 
 /* ----------------------------------------------------------------------------------
